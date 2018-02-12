cede8047fc7ef74f06b1391b9be484ad6259f969
Eric Razor <eric.razorjr@gmail.com>
2018-02-12 05:44:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,23 +14,6 @@ function deepestChild(child){
 }
 
 function increaseRankBy(n){
-  //var theRank = document.querySelectorAll('n')
-  //return theRank
-  
-  const rankedLists = document.querySelectorAll('.ranked-list')
-      const firstList = rankedLists[0]
-      const secondList = rankedLists[1]
-
-      let children = firstList.children
-      let start = 1
-      for (let i = 0, l = children.length; i < l; i++) {
-        parseInt(children[i].innerHTML) === (start + i + 3)
-      }
-
-      children = secondList.children
-      start = 12
-
-      for (let i = 0, l = children.length; i < l; i++) {
-        parseInt(children[i].innerHTML) === (start - i + 3)
-      }
+  var rankedLists = document.querySelectorAll('.ranked-list')
+  return theRank
 }
