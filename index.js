290913b9dfccfbfe652f9d6c97259fb400dfc669
Eric Razor <eric.razorjr@gmail.com>
2018-02-12 04:56:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function getFirstSelector(selector){
 }
 
 function nestedTarget(target){
-  var nested = document.getElementById("");
+  var nested = document.getElementById('nested').getElementBy;
   
 }
