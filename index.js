6e38abe3d0b4a4c153fe8d541e3ae91f32d39540
Eric Razor <eric.razorjr@gmail.com>
2018-02-12 05:09:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(target){
 }
 
 function deepestChild(child){
-  var deepest = document.querySelector(child).innerHTML;
+  var deepest = document.querySelector('deepest').innerHTML;
   return deepest;
 }
